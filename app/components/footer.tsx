b583a250e035d5015375f9db10ad979c3e7aa010
ozboltKlemencic <ozbolt.klemencic+github@gmail.com>
2025-04-15 05:25:40 +0200

    f

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear()
   
   return (
-    <div className='bg-5-custom-footer h-20 flex items-center justify-center'>
+    <div className='bg-5-custom-footer px-4 h-20 flex items-center justify-center'>
       <div className='text-white/70 font-openSans text-center'>
         <p className='text-xs md:text-sm'>
           © {currentYear} Avtomehanika Demšar, Janez Demšar S.P, vse pravice pridržane.
